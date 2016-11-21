1ddb2a849b6733b2f024f67d3ba7c6efdb6bca85
Sergio <campossrg@users.noreply.github.com>
2016-11-21 21:38:35 +0100

    Add sample file

---
+++ preprod.php
@@ -0,0 +1,47 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link href="hhttps://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.0.0/bootstrap-social.css" rel="stylesheet" >
+
+<link rel="stylesheet" type="text/css" href="mystyles.css" media="screen" />
+
+
+
+<div name='dv_post' id=dv_post>
+		<h1>Titulo</h1>
+		<small><i>Posted on ['postDate']</i> by ['postAuthor']</small><br><br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at purus porttitor, commodo nunc vel, pellentesque ligula. Aliquam nec vehicula nibh. Aliquam erat volutpat. Sed eget placerat ligula, eu aliquam neque. Suspendisse a erat et risus maximus scelerisque ac sed enim. Suspendisse sem ex, volutpat eu bibendum semper, accumsan quis ipsum. Duis ornare, nulla ut sodales ultricies, tellus neque posuere diam, id laoreet augue mauris nec felis. Etiam vehicula ac tellus finibus imperdiet. Donec ligula orci, molestie nec eros sit amet, molestie imperdiet risus. </p><br><br>
+		<form method='POST'>
+			<button type='button' class="btn btn-default">Show comments</button><br><br>
+		</form><br><br>
+		<div name=dv_commentaries id=dv_comment>
+			<div class="media">
+				<div class="media-left">
+					<img src="http://www.w3schools.com/bootstrap/img_avatar2.png" class="media-object" style="width:60px">
+				</div>
+				<div class="media-body">
+					<h4 class="media-heading">John Doe <small><i>Posted on February 19, 2016</i></small></h4>
+					<p>Lorem ipsum...</p>
+				</div>
+				<div class="robots-nocontent sd-block sd-social sd-social-icon-text sd-sharing">
+					<h3 class="sd-title">Share this:</h3>
+					<div class="sd-content">
+						<ul>
+							<li class="share-google"><a class="btn btn-social btn-google" href="http://bootsnipp.com/snippets/featured/social-buttons-for-bootstrap#"><span class="fa fa-google">oogle</span></a></li>
+							<li class="share-facebook"><a class="btn btn-block btn-social btn-facebook" href="https://colorlib.com/wp/best-personal-blog-wordpress-themes/?share=facebook&amp;nb=1" target="_blank" title="Click to share on Facebook"><span class="fa fa-facebook">acebook</span></a></li>
+							<li class="share-twitter"><a rel="nofollow" data-shared="sharing-twitter-8891" class="share-twitter sd-button share-icon" href="https://colorlib.com/wp/best-personal-blog-wordpress-themes/?share=twitter&amp;nb=1" target="_blank" title="Click to share on Twitter"><span class="fa fa-twitter"> Twitter </span></a></li>
+							<li class="share-email share-service-visible"><a rel="nofollow" data-shared="" class="share-email sd-button share-icon" href="https://colorlib.com/wp/best-personal-blog-wordpress-themes/?share=email&amp;nb=1" target="_blank" title="Click to email this to a friend"><span class="fa fa-envelope"> Email</span></a></li>
+							<li class="share-end"></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
